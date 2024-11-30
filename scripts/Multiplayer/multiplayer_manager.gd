8d5d3b99e422893dfe2fa5780d451287506eef81
fc56899 <fc56899@alunos.fc.ul.pt>
2024-11-30 18:35:53 +0000

    multiplayer update

--- scripts/Multiplayer/multiplayer_manager.gd
+++ scripts/Multiplayer/multiplayer_manager.gd
@@ -1,28 +1,53 @@
 extends Node
 
+var spawn
+var id_list := []
+
 const SERVER_PORT = 8080
 const SERVER_IP = "127.0.0.1"
 
+var player_mp_scene = preload("res://scenes/Player/player_mp.tscn")
+
 func host():
 	print("host")
+	_add_player(1)
+	
 	var server_peer = ENetMultiplayerPeer.new()
 	server_peer.create_server(SERVER_PORT)
 	
 	multiplayer.multiplayer_peer = server_peer
 	
-	multiplayer.peer_connected.connect(_connect_player)
+	multiplayer.peer_connected.connect(_add_player)
 	multiplayer.peer_disconnected.connect(_remove_player)
 
 func join():
 	print("join")
 	var client_peer = ENetMultiplayerPeer.new()
 	client_peer.create_client(SERVER_IP, SERVER_PORT)
-	
 	multiplayer.multiplayer_peer = client_peer
+	
+	SceneManager.switch_scene("res://scenes/Levels/multiplayer_proto_level.tscn")
 
 
-func _connect_player(id: int):
+func _add_player(id: int):
 	print("Player %s join the game :)" % id)
+	id_list.append(id)
+	
+	if id_list.size() == 2:
+		SceneManager.switch_scene("res://scenes/Levels/multiplayer_proto_level.tscn")
+		spawn_players()
+
+func spawn_players():
+	call_deferred("_deferred_spawn_scene")
+
+func _deferred_spawn_scene():
+	spawn = get_tree().get_current_scene().get_node("Spawn")
+	for id in id_list:
+		var player_mp = player_mp_scene.instantiate()
+		player_mp.player_id = id
+		player_mp.name = str(id)
+		spawn.add_child(player_mp, true)
 
 func _remove_player(id: int):
 	print("Player %s left the game :(" % id)
+	id_list.erase(id)
---
+++ scripts/Multiplayer/player_mp.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@export var player_id := 1:
+	set(id):
+		player_id = id
+
+@onready var sm = $"state machine"
+
+@export var animation_player: AnimationPlayer
+@export var player : CharacterBody3D
+@export var visual : Node3D
+@export var SPEED = 2.6
+@export var SENS = 0.04
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED 
+	
+	# Assign the shared data to the states and initialize state machine
+	for state in sm.get_children():
+		state.animation_player = animation_player
+		state.player = player
+		state.visual = visual
+		state.SPEED = SPEED
+	sm.init()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * SENS))
+
+func _physics_process(delta: float) -> void:
+	#gravity
+	velocity += get_gravity() * delta
+	#move
+	move_and_slide()
