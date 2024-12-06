2c2d1ceedd32c40c795e244b1b955e775f471523
fc56899 <fc56899@alunos.fc.ul.pt>
2024-12-06 12:41:51 +0000

    die update

--- scripts/Player/State Machine/player states/die.gd
+++ scripts/Player/State Machine/player states/die.gd
@@ -3,3 +3,9 @@ extends Player_State
 func enter():
 	animation_player.play("dye")
 	player.queue_free()
+
+func exit_condition() -> bool:
+	return false
+
+func enter_condition() -> bool:
+	return false
